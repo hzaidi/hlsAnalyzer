3e4208d2e8c92a49ce4d09a132f4c4e4e77a15ea
unknown <hzz112@GVMAIN203-126.win.psu.edu>
2015-09-23 18:16:33 -0400

    Code clean up.

--- src/main/java/hls/analyzer/App.java
+++ src/main/java/hls/analyzer/App.java
@@ -26,16 +26,7 @@ public class App
 			MasterPlaylist masterPlayList = new MasterPlaylist(fileName, baseUrl, dataFileArray);
 			reports.addAll(masterPlayList.parse());
 			System.out.println("Finish Parsing.");			
-			new LogWriter(fileName,reports).Createlog();
-			/*for (ValidationReport validationReport : reports) {
-				System.out.println("-----------------------------------------------------");
-				System.out.println("Line Number:" + (validationReport.LineNumber == null ? "" : validationReport.LineNumber));
-				System.out.println("Tag:" + validationReport.ErrorTag);
-				System.out.println("File:" + validationReport.FileName);
-				System.out.println("Details:" + validationReport.Detail);
-				System.out.println("-----------------------------------------------------");
-			}
-			*/		
+			new LogWriter(fileName,reports).Createlog();					
 		}else{
 			System.out.println("Unable to read content from the file specified.");
 		}
